19a0619d05844ba2314da6342306ed92f0fa8848
sushma-b23 <susmaboothu5@gmail.com>
2023-08-18 16:48:00 +0530

    Roboshop catalogue Script

--- catalogue.sh
+++ catalogue.sh
@@ -16,3 +16,6 @@ cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.serv
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
+
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org-shell -y
