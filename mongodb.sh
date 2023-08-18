5bc2cc4ed62aa951c3c5c6bf0e84ede3fa377b5e
sushma-b23 <susmaboothu5@gmail.com>
2023-08-18 15:40:54 +0530

    Roboshop mongodb Script

--- mongodb.sh
+++ mongodb.sh
@@ -3,8 +3,10 @@ cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
 
 yum install mongodb-org -y
 
+systemctl enable mongod
+systemctl start mongod
 
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongodb.conf
 
-systemctl enable mongod
+
 systemctl restart mongod
