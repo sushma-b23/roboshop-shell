711235847c3cbfe67a635efe4b1ab62bde7c1877
sushma-b23 <susmaboothu5@gmail.com>
2023-08-19 22:47:30 +0530

    Create Server.sh

--- create-servers.sh
+++ create-servers.sh
@@ -11,7 +11,6 @@ env=dev
 
  create_ec2() {
   PRIVATE_IP=$(aws ec2 run-instances \
-            --image-id ${AMI_ID} \
             --instance-type t3.micro \
             --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}, {Key=Monitor,Value=yes}]" "ResourseType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]" \
             --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
@@ -32,11 +31,8 @@ else
 }
 
 ##Main Program
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,values=Centos-8-Devops-Practice" | jq '.Images[].ImageId')
 
-
-
- SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+   SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
  if [ -z "${SGID}" ]; then
   echo "Given Security Group does not exit"
   exit 1
