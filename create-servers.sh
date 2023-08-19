95ca13ce6af5dc01fb33da084af4673b4b7150c7
sushma-b23 <susmaboothu5@gmail.com>
2023-08-19 21:48:54 +0530

    Create Server.sh

--- create-servers.sh
+++ create-servers.sh
@@ -10,14 +10,17 @@ env=dev
 
 
  create_ec2() {
- PRIVATE_IP=$(aws ec2 run-instances \
-      --image-id ${AMI_ID} \
-      --instance-type t3.micro \
-      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}, {Key=Monitor,Value=yes}]" "ResourseType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}] \
-       --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
-       --security-group-ids ${SGID} \
-       --iam-instance-profile Name=aws_ssm_dev_role \
-       | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+  PRIVATE_IP=$(aws ec2 run-instances \
+    --image-id ${AMI_ID} \
+    --instance-type t3.micro \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}, {Key=Monitor,Value=yes}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]" \
+    --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
+    --security-group-ids ${SGID} \
+    --iam-instance-profile Name=aws_ssm_dev_role \
+    | jq -r '.Instances[].PrivateIpAddress')
+
+  echo "Private IP address: $PRIVATE_IP"
+
 
  sed -e "s/IpADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" -e "s/DOMAIN/${DOMAIN}/" route53.json >/tmp/record.json
  aws rout53 change-resourse-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json 2>/dev/null
