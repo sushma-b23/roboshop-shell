2fa023ecc1d2578af91709a98a8ee037f8985fb8
sushma-b23 <susmaboothu5@gmail.com>
2023-08-19 21:04:56 +0530

    Create Server.sh

--- create-servers.sh
+++ create-servers.sh
@@ -8,18 +8,19 @@ env=dev
 ##############################
 
 
-create_ec2() {
-PRIVATE_IP=$(aws ec2 run-instances \
+
+ create_ec2() {
+ PRIVATE_IP=$(aws ec2 run-instances \
       --image-id ${AMI_ID} \
       --instance-type t3.micro \
       --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}, {Key=Monitor,Value=yes}]" "ResourseType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}] \
-       --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehaviour=stop}"\
+       --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
        --security-group-ids ${SGID} \
-       iam-instance-profile Name=aws-ssm-dev-role \
-       | jq '.instance[].privateIpAddress' | sed -e  's/"//g')
+       --iam-instance-profile Name=aws_ssm_dev_role \
+       | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
  sed -e "s/IpADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" -e "s/DOMAIN/${DOMAIN}/" route53.json >/tmp/record.json
- aws rout53 change-resourse-record-sets --hosted-zone-id ${ZONE-ID} --change-batch file:///tmp/record.json 2>/dev/null
+ aws rout53 change-resourse-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json 2>/dev/null
  if [ $? -eq 0 ]; then
   echo "Server Created - SUCCESS - DNS RECORD - ${COMPONENT}.${DOMAIN}"
 else
@@ -29,13 +30,13 @@ else
 }
 
 ##Main Program
-AMI_ID=$aws ec2 describe-images --filters "Name=name,values=Centos-8-Devops-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,values=Centos-8-Devops-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 if [ -z "${AMI_ID}" ];then
    echo "AMI_ID not found"
    exit 1
  fi
 
- SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq '.SecurityGroups[].GroupId' \ sed -e 's/"//g')
+ SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
  if [ -z "${SGID}" ]; then
   echo "Given Security Group does not exit"
   exit 1
