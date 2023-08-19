a4fc6bdc8dac19a5c859e85943ac8e950bc129fa
sushma-b23 <susmaboothu5@gmail.com>
2023-08-19 22:48:21 +0530

    Create Server.sh

--- create-servers.sh
+++ create-servers.sh
@@ -22,7 +22,7 @@ env=dev
 
  sed -e "s/IpADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" -e "s/DOMAIN/${DOMAIN}/" route53.json >/tmp/record.json
  aws rout53 change-resourse-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json 2>/dev/null
- if [ $? -eq 0 ]; then
+
   echo "Server Created - SUCCESS - DNS RECORD - ${COMPONENT}.${DOMAIN}"
 else
    echo "Server Created - FAILED - DNS RECORD - ${COMPONENT}.${DOMAIN}"
