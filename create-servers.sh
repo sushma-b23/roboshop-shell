8a4041c7189074072c407b423e74788cd15ba4dd
sushma-b23 <susmaboothu5@gmail.com>
2023-08-20 19:24:21 +0530

    Create Server.sh

--- create-servers.sh
+++ create-servers.sh
@@ -9,7 +9,7 @@ env=dev
 
 create_ec2() {
   PRIVATE_IP=$(aws ec2 run-instances \
-            --image-id ${AMI_ID} \
+
             --instance-type t3.micro \
             --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}, {Key=Monitor,Value=yes}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]" \
             --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
@@ -30,12 +30,6 @@ create_ec2() {
 }
 
 ##Main Program
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-8-Devops-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
-if [ -z "${AMI_ID}" ]; then
-   echo "AMI_ID not found"
-   exit 1
-fi
-
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 if [ -z "${SGID}" ]; then
   echo "Given Security Group does not exist"
