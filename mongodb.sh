3720df0360d09cf803608362c56ad85e570d3530
sushma-b23 <susmaboothu5@gmail.com>
2023-08-21 19:26:07 +0530

    if.sh

--- mongodb.sh
+++ mongodb.sh
@@ -1,20 +1,20 @@
 source common.sh
 print_head "Copy MongoDB Repo file"
-cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${LOG}
 status_check
 
 print_head "Install MongoDB"
-yum install mongodb-org -y
+yum install mongodb-org -y  &>>${LOG}
 status_check
 
 print_head "Update MongoDB Listen Address"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf  &>>${LOG}
 status_check
 
 print_head "Install MongoDB"
-systemctl enable mongod
+systemctl enable mongod  &>>${LOG}
 status_check
 
 print_head "Install MongoDB"
-systemctl restart mongod
+systemctl restart mongod  &>>${LOG}
 status_check
