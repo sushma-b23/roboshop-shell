3960791735c7f0b985ca23720092b76bbb7d3891
sushma-b23 <susmaboothu5@gmail.com>
2023-08-21 19:23:09 +0530

    if.sh

--- catalogue.sh
+++ catalogue.sh
@@ -13,43 +13,44 @@ useradd roboshop &>>${LOG}
 
 
 mkdir -p /app &>>${LOG}
-echo -e "\e[35m Downloading App content\e[0m"
+print_head "Downloading App content"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue                                                                             .zip &>>${LOG}
 status_check
 
-echo -e "\e[35m Cleanup Old Content\e[0m"
+print_head "Cleanup Old Content"
 cd /app &>>${LOG}
 status_check
-echo -e "\e[35m Extracting App Content\e[0m"
+print_head "Extracting App Content"
 rm -rf /app/* &>>${LOG}
 status_check
 unzip /tmp/catalogue.zip &>>${LOG}
 status_check
-echo -e "\e[35m Installing NodeJS Dependencies\e[0m"
+print_head "Installing NodeJS Dependencies"
 cd /app &>>${LOG}
 npm install &>>${LOG}
 status_check
 
-echo -e "\e[35m Configuring Catalogue Service File\e[0m"
+print_head "Configuring Catalogue Service File"
 
 cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.serv                                                                             ice &>>${LOG}
-status_checkecho -e "\e[35m Reload SystemD\e[0m"
+status_check
+print_head "Reload SystemD"
 systemctl daemon-reload &>>${LOG}
 status_check
-echo -e "\e[35m Enable Catalogue Service\e[0m"
+print_head "Enable Catalogue Service"
 systemctl enable catalogue &>>${LOG}
 status_check
-echo -e "\e[35m Start Catalogue Service\e[0m"
+print_head "Start Catalogue Service"
 systemctl start catalogue &>>${LOG}
 status_check
 
-echo -e "\e[35m Configuring Mongo repo\e[0m"
+print_head "Configuring Mongo repo"
 cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${LOG}
 status_check
-echo -e "\e[35m Install Mongo Client\e[0m"
+print_head "Install Mongo Client"
 yum install mongodb-org-shell -y &>>${LOG}
 status_check
 
-echo -e "\e[35m Load Schema\e[0m"
+print_head "Load Schema"
 mongo --host localhost </app/schema/catalogue.js &>>${LOG}
 status_check
--- mongodb.sh
+++ mongodb.sh
@@ -1,11 +1,20 @@
-script_location=$(pwd)
-
+source common.sh
+print_head "Copy MongoDB Repo file"
 cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongo.repo
+status_check
+
+print_head "Install MongoDB"
 yum install mongodb-org -y
+status_check
 
-
-
+print_head "Update MongoDB Listen Address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+status_check
 
+print_head "Install MongoDB"
 systemctl enable mongod
+status_check
+
+print_head "Install MongoDB"
 systemctl restart mongod
+status_check
