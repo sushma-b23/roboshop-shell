0ef8bfcaca82755a01a6101227be428b72bf962b
sushma-b23 <susmaboothu5@gmail.com>
2023-08-22 14:46:19 +0530

    if.sh

--- redis.sh
+++ redis.sh
@@ -14,7 +14,7 @@ status_check
 
 
 print_head "Update Redis Listen Address"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf  &>>${LOG}
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${LOG}
 status_check
 
 print_head "Enable Redis"
