3211a214ac441eaf72ab22e3e530f5ccaace84e9
sushma-b23 <susmaboothu5@gmail.com>
2023-08-30 15:51:20 +0530

    if.sh

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -25,7 +25,10 @@ systemctl start rabbitmq-server &>>${LOG}
 status_check
 
 print_head "Add Application User"
+rabbitmqctl list_users | grep roboshop &>>${LOG}
+if [ $? -ne 0 ]; then
 rabbitmqctl add_user roboshop ${roboshop_rabbitmq_password} &>>${LOG}
+fi
 status_check
 
 print_head "Add Tags to Application User"
